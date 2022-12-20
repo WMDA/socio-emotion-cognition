ba738550d3f87fa34f9260481c4f015a921c6d32
Daniel Halls <p3dh0001@gmail.com>
2022-12-20 14:09:36 +0000

    updated create_task_tsv documentation. Refactored code to remove unecessary function get_files

--- neuroimaging/regressors/create_task_tsv.py
+++ neuroimaging/regressors/create_task_tsv.py
@@ -4,6 +4,17 @@ import pandas as pd
 
 
 def options() -> dict:
+    '''
+    Function to accept accept command line flags
+
+    Parameters
+    ---------
+    None
+
+    Returns
+    -------
+    dictionary of flags given
+    '''
     flags = argparse.ArgumentParser()
     flags.add_argument('-d', '--dir', dest='dir',
                        help='directory where task files are saved')
@@ -14,49 +25,84 @@ def options() -> dict:
     return vars(flags.parse_args())
 
 
-def get_files(directory: str) -> list:
-    return os.listdir(directory)
-
-
 def duration(df: pd.DataFrame) -> pd.Series:
+    '''
+    Function to calculate duration of trial
+
+    Parameters
+    ----------
+    df: pd.Dataframe of trials
+
+    Returns
+    -------
+    duration: pd.Series of duration of trial
+
+    '''
     duration = df['TimeAtStartOfTrial'].shift(
         periods=-1) - df['TimeAtStartOfTrial']
     duration = duration.fillna(duration.mean()).rename('duration')
     return duration
 
 
-def stimuli(col: str, stim: str) -> str:
-    if '4' in col:
+def stimuli(stimuls_shown: str, stim: str) -> str:
+    '''
+    Function to calculate name of stimulus seen
+
+    Parameters
+    ----------
+    stimuls_shown: str of stimulus shown
+    stim: str of if this happy or sad stimulus
+
+    Returns
+    -------
+    returns str of stimulus name
+    '''
+    if '4' in stimuls_shown:
         return f'{stim}'
-    elif '2' in col:
+    elif '2' in stimuls_shown:
         return f'Partially_{stim}'
-    elif '0' in col:
+    elif '0' in stimuls_shown:
         return 'Neutral'
     else:
         return 'Blank'
 
 
 def create_tsv(csv: str, stim: str) -> pd.DataFrame:
-    df = pd.read_csv(csv)
-    dur = duration(df)
-    onset = df['TimeAtStartOfTrial'].rename('onset')
-    trial_type = df['ImageFile'].apply(lambda col: stimuli(col, stim))
-    stim_file = df['ImageFile'].rename('stim_file')
-    response_time = df['RT'].rename('response_time')
-    tsv = pd.concat([onset, dur, trial_type.rename(
+    '''
+    Wrapper function to read in csv file then calculate duration,
+    onset, trial type and stim file
+
+    Parameters
+    ----------
+    csv: str name of csv file
+    stim: str of stimulus type being shown
+
+    Returns
+    -------
+    tsv: pd.Dataframe of BIDS compliant dataframe
+    '''
+    df: pd.DataFrame = pd.read_csv(csv)
+    dur: pd.Series = duration(df)
+    onset: pd.Series = df['TimeAtStartOfTrial'].rename('onset')
+    trial_type: pd.Series = df['ImageFile'].apply(
+        lambda col: stimuli(col, stim))
+    stim_file: pd.Series = df['ImageFile'].rename('stim_file')
+    response_time: pd.Series = df['RT'].rename('response_time')
+    tsv: pd.DataFrame = pd.concat([onset, dur, trial_type.rename(
         'trial_type'), response_time, stim_file], axis=1)
     return tsv
 
 
 if __name__ == '__main__':
-    flags = options()
-    files = get_files(flags['dir'])
+    flags: dict = options()
+    files: list = os.listdir(flags['dir'])
 
     for file in files:
-        csv = flags['dir'] + file
+        csv_location: str = flags['dir'] + file
 
         try:
-            tsv = create_tsv(csv, flags['stim'])
-            print(tsv)
+            tsv: pd.DataFrame = create_tsv(csv_location, flags['stim'])
+
+            print(csv_location.rstrip('.csv'))
         except Exception as e:
             print(e)
