67e94fd46be59d0c37039e36f6ab3312fb681948
Daniel Halls <p3dh0001@gmail.com>
2023-09-21 14:11:21 +0100

    added script to clean fmri images to run on grid

--- resting/preprocessing_pipelines/clean_images_slurm.sh
+++ resting/preprocessing_pipelines/clean_images_slurm.sh
@@ -1,15 +1,22 @@
 #! /bin/bash
-#SBATCH --job-name=first_level_modelling
-#SBATCH --output=/data/project/BEACONB/logs/cleaning_images.log
+#SBATCH --job-name=cleaned_img
+#SBATCH --output=/data/project/BEACONB/logs/%j_cleaning_images.log
 #SBATCH --export=none
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=6
+#SBATCH --array=1-93%4
 #SBATCH --mem=6G
+
 source /software/system/modules/latest/init/bash
 module use /software/system/modules/NaN/generic
 module purge
 module load nan
+
 # Load in script dependent modules here
 module load miniconda/3
 conda activate neuroimaging
 
-python /data/project/BEACONB/task_fmri/socio-emotion-cognition/resting/preprocessing_pipelines/cleaning_images.py
+INDEX=/data/project/BEACONB/task_fmri/socio-emotion-cognition/resting/.resting_img_path
+PARTICIPANT="`awk FNR==$SLURM_ARRAY_TASK_ID $INDEX`"
+echo "Running on $HOSTNAME"
+echo $PARTICIPANT
+python /data/project/BEACONB/task_fmri/socio-emotion-cognition/resting/preprocessing_pipelines/grid_clean_script -s $PARTICIPANT
