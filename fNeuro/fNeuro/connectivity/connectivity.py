cf9db6bbfffb17348147d68bd2c95699288983be
Daniel Halls <p3dh0001@gmail.com>
2023-09-28 16:11:18 +0100

    added in Cyclic analysis class

--- fNeuro/fNeuro/connectivity/connectivity.py
+++ fNeuro/fNeuro/connectivity/connectivity.py
@@ -1,63 +1,83 @@
 import numpy as np
 import cyclicityanalysis.orientedarea as cao
 import pandas as pd
+from math import sqrt
 
-def vectorize(symmetric: np.array) -> np.array:
-    '''
-    Nilearn's vectorize to return the lower triangluation of a matrix
-    in 1D format.
+class Cyclic_analysis:
+    def __init__(self, to_vectorize=True) -> None:
+        '''
+        Class to run Cyclic analysis on time series data
 
-    Parameters
-    ----------
-    symmetric: np.array 
-        Correlation matrix
+        Parameters
+        ----------
+        to_vectorize: bool (default is true)
+            If true then will do nilearn Connectivity measure type vectorization
+
+        Return
+        ------
+        None
+
+        '''
+        self.to_vectorize = to_vectorize
+
+    def vectorize(self, symmetric: np.array) -> np.array:
+        '''
+        Nilearn's vectorize to return the lower triangluation of a matrix
+        in 1D format.
     
-    Returns
-    -------
-    np.array: 1D numpy array
-        array of lower triangluation of a matrix
-    '''
-    scaling = np.ones(symmetric.shape[-2:])
-    np.fill_diagonal(scaling, sqrt(2.0))
-    tril_mask = np.tril(np.ones(symmetric.shape[-2:])).astype(bool)
-    return symmetric[..., tril_mask] / scaling[tril_mask]
+        Parameters
+        ----------
+        symmetric: np.array 
+            Correlation matrix
+        
+        Returns
+        -------
+        np.array: 1D numpy array
+            array of lower triangluation of a matrix
+        '''
+        scaling = np.ones(symmetric.shape[-2:])
+        np.fill_diagonal(scaling, sqrt(2.0))
+        tril_mask = np.tril(np.ones(symmetric.shape[-2:])).astype(bool)
+        return symmetric[..., tril_mask] / scaling[tril_mask]
 
-def individual_cyclic_analysis(time_series: np.array) -> np.array:
+    def cyclic_analysis(self, time_series: np.array) -> np.array:
 
-    '''
-    Function to perform cyclic analysis on an individual time series
+        '''
+        Function to perform cyclic analysis on an individual time series
+        
+        Parameters
+        ----------
+        time_series: np.array
+            time series
+        
+        Returns
+        -------
+        np.array: 1D numpy array
+            array of lower triangluation of a matrix
+        '''
     
-    Parameters
-    ----------
-    time_series: np.array
-        time series
+        df = pd.DataFrame(time_series)
+        oriented_area = cao.OrientedArea(df)
+        lead_lag_df = oriented_area.compute_lead_lag_df()
+        if self.to_vectorize == True:
+            return self.vectorize(lead_lag_df.values)
+        else:
+            return lead_lag_df.values
     
-    Returns
-    -------
-    np.array: 1D numpy array
-        array of lower triangluation of a matrix
-    '''
-
-    df = pd.DataFrame(time_series)
-    oriented_area = cao.OrientedArea(df)
-    lead_lag_df = oriented_area.compute_lead_lag_df()
-    return vectorize(lead_lag_df.values)
-
-
-def cyclic_analysis(time_series: np.array) -> np.array:
+    def fit(self, time_series: np.array) -> np.array:
     
-    '''
-    Function to perform a cyclic analysis of time series
-
-    Parameters
-    ----------
-    time_series: np.array
-        time series
+        '''
+        Function to perform a cyclic analysis of time series
     
-    Returns
-    -------
-    np.array: 1D numpy array
-        array of lower triangluation of a matrix
-    '''
-    
-    return np.array(list(map(individual_cyclic_analysis, time_series)))
+        Parameters
+        ----------
+        time_series: np.array
+            time series
+        
+        Returns
+        -------
+        np.array: 1D numpy array
+            array of lower triangluation of a matrix
+        '''
+        
+        return np.array(list(map(self.cyclic_analysis, time_series)))
